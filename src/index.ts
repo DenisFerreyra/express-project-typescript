fcc46fde72cf382e0cdf733f533e789c42738302
DenisFerreyra <denis.ferreyra@gigigo.com>
2023-02-06 16:42:59 -0300

    [Comments]: add comments & install axios

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 //Libraries
 import express from "express";
+import axios from "axios";
+
 const app = express();
-app.use(express.json());                //middleware que transforma la req.body en un json
+app.use(express.json()); //middleware que transforma la req.body en un json
 
 const PORT = 3000;
 
-app.get("/status", (_req, res) => {                //Si marca error de que no se está usando una variable, ponemos guión bajo, o guión bajo + nombre y lo ignora
-  console.log(`Le pego aquí!`);
-
+app.get("/status", (_req, res) => {
+  //Si marca error de que no se está usando una variable, ponemos guión bajo, o guión bajo + nombre y lo ignora
   res.send("Project initialized.");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    
-})
+  console.log(`Server running on port ${PORT}`); //Invocamos al método listen de app para levantar el servidor en el puerto indicado
+});
