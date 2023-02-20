6bcf220cf3f55e15f5e1c2a79a9ee18a5522aabd
DenisFerreyra <denis.ferreyra@gigigo.com>
2023-02-20 13:57:13 -0300

    [Feature]: add get & create animal

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 //Libraries
 import express from "express";
 import axios from "axios";
+import mongoose from "mongoose";
+
+//Para trabajar con docker crearemos un modelo de mongo con la siguientes características
+const Animal = mongoose.model(
+  "Animal",
+  new mongoose.Schema({
+    tipo: String,
+    estado: String,
+  })
+);
 
 const app = express();
 app.use(express.json()); //middleware que transforma la req.body en un json
+
+//Aquí se le indica el usuario (demis), la contraseña (password), el host (localhost), el puerto (27012)
+//luego la base de datos (miapp) y por último el tipo de usuario (admin)
+mongoose.connect(
+  "mongodb://denis:password@localhost:27017/miapp?authSource=admin"
+);
 
 const PORT = 3000;
 
@@ -11,6 +27,19 @@ app.get("/status", (_req, res) => {
   //Si marca error de que no se está usando una variable, ponemos guión bajo, o guión bajo + nombre y lo ignora
   res.send("Project initialized.");
 });
+
+//Acá traemos la lista de animales de la base de datos
+app.get("/animals", async (_req, res) => {
+  console.log("listando animales");
+  const animales = await Animal.find();
+  return res.send(animales);
+});
+
+//Acá si bien usamos un get, es para no usar el postman y poder hacerlo desde el navegador
+app.get("/crear-animal", async (_req, res) => {
+  console.log('creando animal');
+  
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`); //Invocamos al método listen de app para levantar el servidor en el puerto indicado
